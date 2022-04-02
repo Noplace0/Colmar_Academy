56d17c6a03db82ba2c40b554a1ecb325ca6206a2
fidio <fidio@live.hk>
2022-04-02 14:27:46 +0800

    initial commit

---
+++ tailwind.config.js
@@ -0,0 +1,16 @@
+module.exports = {
+  content: ['./*.html',  ],
+  theme: {
+    extend: {
+      colors: {
+      'turquoise': '#4cbfa6',
+      'softLilac': '#f6ebf4',
+      'hookpurple': '#482673',
+      'orchid': '#ed0b70',
+      'digiDenim': '#023B59',
+    },
+  }
+  },
+  plugins: [],
+  darkMode: 'class',
+}
